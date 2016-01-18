cb8dce904d5074e0186fb3f1ab6f0e1d2d1453f8
matlon <mattias.lonnerberg@gmail.com>
2016-01-18 20:53:06 +0100

    mouse over block indicator
    fun stuff

---
+++ Assets/FloatingEffect.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatingEffect : MonoBehaviour {
+    private float time = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        time = time % ((2/3.5f) * Mathf.PI);
+        transform.position = new Vector3(transform.position.x, 0.6f + (0.05f * Mathf.Sin(3.5f*time)), transform.position.z);
+	}
+}
--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,16 +4,22 @@ using System.Collections;
 public class Block : MonoBehaviour {
 
 	public bool isWalkable, isBlocked;
+    public bool hasMouseOver;
+    public GameObject marker;
 
 	// Use this for initialization
 	void Start () {
-		
+        marker = GameObject.Find("Selection Marker");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
+
+    void OnMouseEnter ()
+    {
+        marker.transform.position = new Vector3(transform.position.x, marker.transform.position.y, transform.position.z);
+    }
 
 	public bool CheckAvailable()
 	{
