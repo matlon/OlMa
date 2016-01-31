87bd4fcf3b44e2f035181be895f7a861873f1bb1
matlon <mattias.lonnerberg@gmail.com>
2016-01-31 18:00:43 +0100

    Folders and MovementSet Hierarchy
    Ordered folders
    Can now add new scripts for different piece types that inherits from
    MovementSet

---
+++ Assets/Scripts/PieceMovement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PieceMovement : MonoBehaviour {
+
+    public MovementSet ms;
+    private Vector2[][] movementSet;
+
+    // Use this for initialization
+    void Start() {
+        ms = GetComponent<MovementSet>();
+        movementSet = ms.getMovementSet();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public ArrayList GetValidMoves(Block[,] board, int cbx, int cbz)
+    {
+        ArrayList availableMoves = new ArrayList();
+        foreach (Vector2[] vec in movementSet)
+        {
+            for (int i = 0; i < vec.Length; i++)
+            {
+                if (isValidIndex(board, cbx + (int)vec[i].x, cbz + (int)vec[i].y) && board[cbx + (int)vec[i].x, cbz + (int)vec[i].y].CheckAvailable())
+                {
+                    availableMoves.Add(board[cbx + (int)vec[i].x, cbz + (int)vec[i].y]);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        return availableMoves;
+    }
+
+    private bool isValidIndex(Block[,] board, int x, int z)
+    {
+        int maxX = board.GetUpperBound(0);
+        int maxZ = board.GetUpperBound(1);
+        return 0 <= x && 0 <= z && x <= maxX && z <= maxZ;
+    }
+
+    private Vector2 v (int x, int y)
+    {
+        return new Vector2(x, y);
+    }
+
+}
