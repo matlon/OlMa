cf7790ca85a4fcdc8d743318b75b75cfa5919c6d
matlon <mattias.lonnerberg@gmail.com>
2016-01-31 18:56:40 +0100

    Basic Drag Camera Movement
    basic click and drag camera movement implemented

---
+++ Assets/CameraDrag.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraDrag : MonoBehaviour {
+
+    public float speed = 10;
+    public float minMove = 0.05f;
+
+    private Vector3 origin;
+    private bool mouseDown, mouseMovement;
+    private Vector3 pos;
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            origin = Input.mousePosition;
+            mouseDown = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            mouseDown = false;
+            mouseMovement = false;
+        }
+        else if (!mouseMovement && mouseDown && Camera.main.ScreenToViewportPoint(Input.mousePosition - origin).magnitude > minMove)
+        {
+            pos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            mouseMovement = true;
+        }
+
+        if (mouseMovement)
+        {
+            Vector3 oldPos = pos;
+            pos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            //Vector3 move = new Vector3(pos.x * speed, 0, pos.y * speed);
+
+            transform.Translate(speed * (oldPos - pos), Space.Self);
+        }
+
+	}
+}
