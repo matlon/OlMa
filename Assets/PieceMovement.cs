dd8c67601f169f9e7edc6dcd1bbeaa7082a5e323
matlon <mattias.lonnerberg@gmail.com>
2016-01-31 16:32:55 +0100

    New Storage implementation
    first attempt

---
+++ Assets/PieceMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PieceMovement : MonoBehaviour {
+
+    Vector2[,] movementSet;
+
+	// Use this for initialization
+	void Start () {
+        Vector2[,] m =  {
+                            { v(1, 2) },
+                            { v(-1, 2) },
+                            { v(1, -2) },
+                            { v(-1, -2) },
+                            { v(2, 1) },
+                            { v(-2, 1) },
+                            { v(2, -1) },
+                            { v(-2, -1) }
+                        };
+        movementSet = m;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+    
+    private Vector2 v (int x, int y)
+    {
+        return new Vector2(x, y);
+    }
+
+}
