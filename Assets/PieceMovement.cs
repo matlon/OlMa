7d867b17664c3f14d1f15f3be54bf85f0aee7b03
matlon <mattias.lonnerberg@gmail.com>
2016-01-31 16:37:42 +0100

    update for jagged movement matrices

--- Assets/PieceMovement.cs
+++ Assets/PieceMovement.cs
@@ -3,19 +3,19 @@ using System.Collections;
 
 public class PieceMovement : MonoBehaviour {
 
-    Vector2[,] movementSet;
+    Vector2[][] movementSet;
 
-	// Use this for initialization
-	void Start () {
-        Vector2[,] m =  {
-                            { v(1, 2) },
-                            { v(-1, 2) },
-                            { v(1, -2) },
-                            { v(-1, -2) },
-                            { v(2, 1) },
-                            { v(-2, 1) },
-                            { v(2, -1) },
-                            { v(-2, -1) }
+    // Use this for initialization
+    void Start() {
+        Vector2[][] m = new Vector2[][] {
+                            new Vector2[] { v( 1, 1), v( 2, 2) },
+                            new Vector2[] { v(-1, 2) },
+                            new Vector2[] { v( 1,-2) },
+                            new Vector2[] { v(-1,-2) },
+                            new Vector2[] { v( 2, 1) },
+                            new Vector2[] { v(-2, 1) },
+                            new Vector2[] { v( 2,-1) },
+                            new Vector2[] { v(-2,-1) }
                         };
         movementSet = m;
     }
